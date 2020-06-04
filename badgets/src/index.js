0730388dda902b6f9fee513d756e77cc6e51cc1c
Daniel R <raiga1234@gmail.com>
2020-06-03 19:16:16 -0500

    understanding jsx

--- badgets/src/index.js
+++ badgets/src/index.js
@@ -8,7 +8,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+// const jsx = <h1>Hello, React Badges!</h1>;
+// const element = React.createElement(
+//   'a',
+//   { href: 'https://google.com' },
+//   'Ir a Google'
+// );
+const name = 'Daniel';
+// const element = React.createElement('h1', {}, `Hola, soy ${name}`);
+// const jsx = <h1>Hola soy, {name}</h1>;
+
+const jsx = (
+  <div>
+    <h1>Hola, soy daniel</h1>
+    <p>Agilista</p>
+  </div>
+);
+
+const element = React.createElement(
+  'div',
+  {},
+  React.createElement('h1', {}, 'Hola, soy Daniel'),
+  React.createElement('p', {}, 'Agilista!!!')
+);
 
 const container = document.getElementById('app');
 
