133f3301c7e2adb6603575f9254c280528e84c4a
Daniel <raiga1234@gmail.com>
2020-06-05 16:49:57 -0500

    Pages in React

--- badgets/src/index.js
+++ badgets/src/index.js
@@ -3,18 +3,9 @@ import ReactDOM from "react-dom";
 
 import "./global.css";
 import "bootstrap/dist/css/bootstrap.css";
-import Badge from "./components/Badge";
+// import Badge from "./components/Badge";
+import BadgeNew from "./pages/BadgeNew";
 
 const container = document.getElementById("app");
 
-// ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(
-  <Badge
-    firstName="Daniel"
-    lastName="Romero"
-    jobTitle="Agilista"
-    twitter="leolplex"
-    avatarUrl="https://avatars2.githubusercontent.com/u/11394398?s=400&u=ac26727749edee2cc3424428cbaaa5e67819f9fa&v=4"
-  />,
-  container
-);
+ReactDOM.render(<BadgeNew />, container);
---
+++ badgets/src/pages/BadgeNew.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import "./styles/BadgeNew.css";
+import header from "../images/badge-header.svg";
+import Navbar from "../components/Navbar";
+import Badge from "../components/Badge";
+
+class BadgeNew extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="Logo"></img>
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Badge
+                firstName="Daniel"
+                lastName="Romero"
+                jobTitle="Agilista"
+                twitter="leolplex"
+                avatarUrl="https://avatars2.githubusercontent.com/u/11394398?s=400&u=ac26727749edee2cc3424428cbaaa5e67819f9fa&v=4"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BadgeNew;
