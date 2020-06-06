3bfbfd52a43a3376d795f325fe1ab746d6571e8d
Daniel <raiga1234@gmail.com>
2020-06-06 14:24:32 -0500

    BrowserRouter, Switch, Route in React JS

--- badgets/src/pages/Badges.js
+++ badgets/src/pages/Badges.js
@@ -4,6 +4,7 @@ import "./styles/Badges.css";
 import ConfLogo from "../images/badge-header.svg";
 import Navbar from "../components/Navbar";
 import BadgesList from "../components/BadgesList";
+import { Link } from "react-router-dom";
 
 class Badges extends React.Component {
   state = {
@@ -59,9 +60,9 @@ class Badges extends React.Component {
         </div>
         <div className="Badges__container">
           <div className="Badges__buttons">
-            <a href="/badges/new" className="btn btn-primary">
+            <Link to="/badges/new" className="btn btn-primary">
               New Badge
-            </a>
+            </Link>
           </div>
           <div className="Badges__list">
             <div className="Badges__container">
