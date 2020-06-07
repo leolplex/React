8ef414c4755106a5fb692b5642ad0d7fcb338b0d
Daniel <raiga1234@gmail.com>
2020-06-07 12:06:57 -0500

    API local calls in React JS

--- badgets/src/components/BadgesList.js
+++ badgets/src/components/BadgesList.js
@@ -2,22 +2,34 @@ import React from "react";
 
 import "./styles/BadgesList.css";
 import BadgesListItem from "../components/BadgesListItem";
+import { Link } from "react-router-dom";
 
 class BadgesList extends React.Component {
   render() {
-    return (
-      <div className="BadgesList">
-        <ul className="list-unstyled">
-          {this.props.badges.map((badge) => {
-            return (
-              <li key={badge.id}>
-                <BadgesListItem badge={badge} />
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
+    if (this.props.badges.length > 0) {
+      return (
+        <div className="BadgesList">
+          <ul className="list-unstyled">
+            {this.props.badges.map((badge) => {
+              return (
+                <li key={badge.id}>
+                  <BadgesListItem badge={badge} />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h3>No badges were found</h3>
+          <Link className="btn btn-primary" to="/badges/new">
+            Create a new badge
+          </Link>
+        </div>
+      );
+    }
   }
 }
 
