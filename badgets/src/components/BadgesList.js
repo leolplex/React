ccd0891364ebae88c6c3384c8d7e913dca63b12a
Daniel <raiga1234@gmail.com>
2020-06-13 20:33:49 -0500

    Custom Hook filter in React JS

--- badgets/src/components/BadgesList.js
+++ badgets/src/components/BadgesList.js
@@ -1,40 +1,80 @@
-import React from "react";
+import React from 'react';
 
-import "./styles/BadgesList.css";
-import BadgesListItem from "../components/BadgesListItem";
-import { Link } from "react-router-dom";
+import './styles/BadgesList.css';
+import BadgesListItem from '../components/BadgesListItem';
+import { Link } from 'react-router-dom';
 
-class BadgesList extends React.Component {
-  render() {
-    if (this.props.badges.length > 0) {
-      return (
-        <div className="BadgesList">
-          <ul className="list-unstyled">
-            {this.props.badges.map((badge) => {
-              return (
-                <li key={badge.id}>
-                  <Link
-                    className="text-reset text-decoration-none"
-                    to={`badges/${badge.id}`}
-                  >
-                    <BadgesListItem badge={badge} />
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
+function useSearchBadges(badges) {
+  const [query, setQuery] = React.useState('');
+  const [filteredBadges, setFilteredBadges] = React.useState(badges);
+
+  React.useMemo(() => {
+    const result = badges.filter((badge) => {
+      return `${badge.firstName} ${badge.lastName}`
+        .toLowerCase()
+        .includes(query.toLowerCase());
+    });
+
+    setFilteredBadges(result);
+  }, [badges, query]);
+
+  return { query, setQuery, filteredBadges };
+}
+
+function BadgesList(props) {
+  const badges = props.badges;
+  const { query, setQuery, filteredBadges } = useSearchBadges(badges);
+
+  if (filteredBadges.length > 0) {
+    return (
+      <div className="BadgesList">
+        <div className="form-group">
+          <label>Filter Badges</label>
+          <input
+            type="text"
+            className="form-control"
+            value={query}
+            onChange={(e) => {
+              setQuery(e.target.value);
+            }}
+          ></input>
         </div>
-      );
-    } else {
-      return (
-        <div>
-          <h3>No badges were found</h3>
-          <Link className="btn btn-primary" to="/badges/new">
-            Create a new badge
-          </Link>
+        <ul className="list-unstyled">
+          {filteredBadges.map((badge) => {
+            return (
+              <li key={badge.id}>
+                <Link
+                  className="text-reset text-decoration-none"
+                  to={`badges/${badge.id}`}
+                >
+                  <BadgesListItem badge={badge} />
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <div className="form-group">
+          <label>Filter Badges</label>
+          <input
+            type="text"
+            className="form-control"
+            value={query}
+            onChange={(e) => {
+              setQuery(e.target.value);
+            }}
+          ></input>
         </div>
-      );
-    }
+        <h3>No badges were found</h3>
+        <Link className="btn btn-primary" to="/badges/new">
+          Create a new badge
+        </Link>
+      </div>
+    );
   }
 }
 
